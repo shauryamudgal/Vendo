b7af989332ed15112f12a7d149a785f1474f2b55
rudra-pratapsingh <rudra20.sre@gmail.com>
2025-12-01 15:40:14 +0530

    Created stock schemas; stock routers in progress

---
+++ backend/routes/stock_router.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends, HTTPExcpetion
+from backend.database.db import get_db
+from backend.models.models import Items
+from backend.schemas.stock_schemas import Stock, LowStockItems, UpdateStock, UpdatedStockInfo, ItemStock
+
+import logging
+
+router = APIRouter(prefix="/stock", tags=["Stock"])
+
+logging.basicConfig(level = logging.INFO)
+logger = logging.getLogger(__name__)
+
+@router.get('/', response_model=Stock)
+def get_all_stock(user_id: int, db: Session = Depends(get_db)):
+  try:
+    items = []
+    items.append(db.query(Items).filter(Items.user_id == user_id).all())
+    count = len(items)
+    return Stock(
+      count = count,
+      items = items
+    )
+  
+  except Exception as e:
+    logger.error(f"Error while fetching stock {str(e)}")
+    raise HTTPExcpetion(
+      status_code = 500,
+      detail = f"An error occured while fetching stock: {str(e)}"
+    )
---
+++ backend/schemas/stock_schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from typing import List
+
+class UpdateStock(BaseModel):
+  item_id: int
+  new_stock: int
+
+class UpdatedStockInfo(BaseModel):
+  status: str
+  message: str
+  updated_item_details: List[UpdateStock]
+
+class ItemStock(BaseModel):
+  id: int
+  name: str
+  category: str
+  current_stock: int
+
+class Stock(BaseModel):
+  count: int
+  items: List[ItemStock]
+
+class LowStockItems(BaseModel):
+  threshold: int
+  count: int
+  low_stock_items: List[ItemStock]
